4cc776fb217db9796acb9b0a4c14bd496a09468b
Abdalrahman Eissa <abdalrahmaneissaa@proton.me>
2025-07-12 16:33:52 +0300

    Remove support from another devices (only support alioth)

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alioth apollo cas cmi dagu elish enuma lmi munch pipa psyche thyme umi,$(TARGET_DEVICE)),)
+ifneq ($(filter alioth,$(TARGET_DEVICE)),)
 
 endif
